38a456895c65eea306787bc5e15f6f42715c9a54
Shalom Dahan <shalom.dahan@icloud.com>
2025-04-30 19:38:37 +0300

    add lambda deployment

--- LambdaApiCICD/Program.cs
+++ LambdaApiCICD/Program.cs
@@ -36,6 +36,13 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapGet("/", () =>
+{
+    
+    return "Hello";
+})
+.WithOpenApi();
+
 app.Run();
 
 internal record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
