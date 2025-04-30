cba907de83c264212a8cb62d3be6e176547ed61e
Shalom Dahan <shalom.dahan@icloud.com>
2025-04-30 19:59:24 +0300

    add AddAWSLambdaHosting

--- LambdaApiCICD/Program.cs
+++ LambdaApiCICD/Program.cs
@@ -5,6 +5,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddAWSLambdaHosting(LambdaEventSource.HttpApi);
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
